6daa5285e4ffa10c02a8800e49ebaebbaade9160
rgo555 <rafa.jere@hotmail.com>
2023-03-04 01:33:34 +0100

    test

---
+++ Assets/GrowVine.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowVine : MonoBehaviour
+{
+    public List<MeshRenderer> growVinesMeshes;
+    public float timwtoGrow = 5;
+    public float refreshRate = 0.05f;
+    [Range(0.1)]
+    public float minGorw = 0.2;
+    [Range(0,1)]
+    public float maxGrow = 0.97f;
+
+    private List<Material> growVinesMaterials = new List<Material>();
+    private bool fullyGrown;
+
+    void Start()
+    {
+        for(int i=0; i<growVinesMeshes.Count; i++)
+        {
+            for(int j=0; j<growVinesMeshes[i].materials.Length; j++)
+            {
+                if(growVinesMeshes[i].materials[j].HasProperty("Grow_"))
+                {
+                    growVinesMeshes[i].materials[j].SetFloat("Grow_", miniGrow);
+                    growVinesMaterials.Add(growVinesMeshes[i].materials[j]);
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            for(int i=0; i<growVinesMaterials.Count; i++)
+            {
+                StartCoroutine(GrowVines(growVinesMaterials[i]));
+            }
+        }
+    }
+
+    IEnumerator GrowVines (Material mat)
+    {
+        float growValue = mat.GetFloat("Grow_");
+
+        if(!fullyGrown)
+        {
+            while(grownValue < maxGrow)
+            {
+                grownValue += 1/(timeToGrow/refreshRate);
+                mat.SetFloat("Grow_", growValue);
+
+                yield return new WaitForSeconds(refreshRate);
+            }
+        }
+        else
+        {
+            while(grownValue > minGrow)
+            {
+                grownValue -= 1/(timeToGrow/refreshRate);
+                mat.SetFloat("Grow_", growValue);
+
+                yield return new WaitForSeconds(refreshRate);
+            }
+        }
+
+        if(growValues >= maxGrow)
+            fullyGrown = true;
+        else
+            fullyGrown = false;
+    }
+}
